f3199fd492befb78670ad7c0d38fc611aee057fa
Christian Legnitto <christian@legnitto.com>
2025-11-24 10:30:32 -0800

    Use upstream rspirv and keep conditional extension support

--- rust/spirv-tools-core/src/disassembly/mod.rs
+++ rust/spirv-tools-core/src/disassembly/mod.rs
@@ -1,5 +1,5 @@
-use rspirv::binary::{Disassemble, Parser};
-use rspirv::dr::{self, Instruction, Loader, ModuleHeader, Operand};
+use rspirv::binary::{Disassemble, ParseAction, Parser};
+use rspirv::dr::{self, Block, Function, Instruction, Module, ModuleHeader, Operand};
 use rspirv::grammar::{GlslStd450InstructionTable, OpenCLStd100InstructionTable};
 use rspirv::spirv;
 use std::collections::{HashMap, HashSet};
@@ -103,7 +103,7 @@ pub fn disassemble_binary(
     let formatting =
         FormattingOptions::try_from(requested).map_err(DisassemblyError::Unsupported)?;
 
-    let mut loader = Loader::new();
+    let mut loader = ExtendedLoader::new();
     match Parser::new(words_as_bytes(words), &mut loader).parse() {
         Ok(()) => {}
         Err(rspirv::binary::ParseState::OpcodeUnknown(_, _, _)) => {
@@ -111,7 +111,7 @@ pub fn disassemble_binary(
         }
         Err(error) => return Err(DisassemblyError::parse_error(error.to_string())),
     }
-    let mut module = loader.module();
+    let mut module = loader.into_module();
     update_module_header(&mut module, words);
     let offsets = collect_instruction_offsets(words);
     let mut text = render_module_text(&module, words, &offsets, &formatting);
@@ -272,6 +272,137 @@ impl ValueTypeTable {
 
     fn get(&self, id: u32) -> Option<&TypeInfo> {
         self.entries.get(&id)
+    }
+}
+
+struct ExtendedLoader {
+    module: Module,
+    function: Option<Function>,
+    block: Option<Block>,
+}
+
+impl ExtendedLoader {
+    fn new() -> Self {
+        Self {
+            module: Module::new(),
+            function: None,
+            block: None,
+        }
+    }
+
+    fn into_module(self) -> Module {
+        self.module
+    }
+}
+
+macro_rules! if_ret_err {
+    ($condition: expr, $error: expr) => {
+        if $condition {
+            return ParseAction::Error(Box::new($error));
+        }
+    };
+}
+
+impl rspirv::binary::Consumer for ExtendedLoader {
+    fn initialize(&mut self) -> ParseAction {
+        ParseAction::Continue
+    }
+
+    fn finalize(&mut self) -> ParseAction {
+        if_ret_err!(self.block.is_some(), rspirv::dr::Error::UnclosedBlock);
+        if_ret_err!(self.function.is_some(), rspirv::dr::Error::UnclosedFunction);
+        ParseAction::Continue
+    }
+
+    fn consume_header(&mut self, header: ModuleHeader) -> ParseAction {
+        self.module.header = Some(header);
+        ParseAction::Continue
+    }
+
+    fn consume_instruction(&mut self, inst: Instruction) -> ParseAction {
+        let opcode = inst.class.opcode;
+        match opcode {
+            spirv::Op::Capability => self.module.capabilities.push(inst),
+            spirv::Op::Extension | spirv::Op::ConditionalExtensionINTEL => {
+                self.module.extensions.push(inst)
+            }
+            spirv::Op::ExtInstImport => self.module.ext_inst_imports.push(inst),
+            spirv::Op::MemoryModel => self.module.memory_model = Some(inst),
+            spirv::Op::EntryPoint => self.module.entry_points.push(inst),
+            spirv::Op::ExecutionMode => self.module.execution_modes.push(inst),
+            spirv::Op::String
+            | spirv::Op::SourceExtension
+            | spirv::Op::Source
+            | spirv::Op::SourceContinued => self.module.debug_string_source.push(inst),
+            spirv::Op::Name | spirv::Op::MemberName => self.module.debug_names.push(inst),
+            spirv::Op::ModuleProcessed => self.module.debug_module_processed.push(inst),
+            opcode if rspirv::grammar::reflect::is_location_debug(opcode) => {
+                match &mut self.block {
+                    Some(block) => block.instructions.push(inst),
+                    None => self.module.types_global_values.push(inst),
+                }
+            }
+            opcode if opcode.is_annotation() => self.module.annotations.push(inst),
+            opcode if opcode.is_type() || opcode.is_constant() => {
+                self.module.types_global_values.push(inst)
+            }
+            spirv::Op::Variable if self.function.is_none() => {
+                self.module.types_global_values.push(inst)
+            }
+            spirv::Op::Undef if self.function.is_none() => {
+                self.module.types_global_values.push(inst)
+            }
+            spirv::Op::Function => {
+                if_ret_err!(self.function.is_some(), rspirv::dr::Error::NestedFunction);
+                let mut func = Function::new();
+                func.def = Some(inst);
+                self.function = Some(func);
+            }
+            spirv::Op::FunctionEnd => {
+                if_ret_err!(
+                    self.function.is_none(),
+                    rspirv::dr::Error::MismatchedFunctionEnd
+                );
+                if_ret_err!(self.block.is_some(), rspirv::dr::Error::UnclosedBlock);
+                self.function.as_mut().unwrap().end = Some(inst);
+                self.module.functions.push(self.function.take().unwrap());
+            }
+            spirv::Op::FunctionParameter => {
+                if_ret_err!(
+                    self.function.is_none(),
+                    rspirv::dr::Error::DetachedFunctionParameter
+                );
+                self.function.as_mut().unwrap().parameters.push(inst);
+            }
+            spirv::Op::Label => {
+                if_ret_err!(self.function.is_none(), rspirv::dr::Error::DetachedBlock);
+                if_ret_err!(self.block.is_some(), rspirv::dr::Error::NestedBlock);
+                let mut block = Block::new();
+                block.label = Some(inst);
+                self.block = Some(block);
+            }
+            opcode if rspirv::grammar::reflect::is_block_terminator(opcode) => {
+                if_ret_err!(
+                    self.block.is_none(),
+                    rspirv::dr::Error::MismatchedTerminator
+                );
+                self.block.as_mut().unwrap().instructions.push(inst);
+                self.function
+                    .as_mut()
+                    .unwrap()
+                    .blocks
+                    .push(self.block.take().unwrap());
+            }
+            _ => {
+                if self.block.is_none() {
+                    return ParseAction::Error(Box::new(rspirv::dr::Error::DetachedInstruction(
+                        Some(inst),
+                    )));
+                }
+                self.block.as_mut().unwrap().instructions.push(inst);
+            }
+        }
+        ParseAction::Continue
     }
 }
 
--- third_party/rspirv/autogen/README.md
+++
@@ -1,5 +0,0 @@
-Automatic code generation of the rspirv project
-=============================
-
-This directory contains the code to generate various Rust code snippets
-used in other crates from SPIR-V's JSON grammar.
