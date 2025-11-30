cf318437da1da37dff43bac3dde45a6fe0022383
Christian Legnitto <christian@legnitto.com>
2025-11-29 17:51:03 -0800

    Add version gating tests for Vulkan memory model pointer accesses

--- rust/spirv-tools-core/src/validation/mod.rs
+++ rust/spirv-tools-core/src/validation/mod.rs
@@ -21009,6 +21009,62 @@ mod tests {
     }
 
     #[test]
+    fn memory_access_make_pointer_available_load_requires_spirv_1_5() {
+        use rspirv::{binary::Assemble, dr::Builder};
+
+        let mut builder = Builder::new();
+        builder.set_version(1, 4);
+        builder.capability(rspirv::spirv::Capability::Shader);
+        builder.memory_model(
+            rspirv::spirv::AddressingModel::Logical,
+            rspirv::spirv::MemoryModel::GLSL450,
+        );
+
+        let void = builder.type_void();
+        let uint = builder.type_int(32, 0);
+        let ptr = builder.type_pointer(None, rspirv::spirv::StorageClass::Workgroup, uint);
+        let function_type = builder.type_function(void, std::iter::empty::<u32>());
+        let var = builder.variable(ptr, None, rspirv::spirv::StorageClass::Workgroup, None);
+
+        builder
+            .begin_function(
+                void,
+                None,
+                rspirv::spirv::FunctionControl::NONE,
+                function_type,
+            )
+            .unwrap();
+        builder.begin_block(None).unwrap();
+        let scope = builder.constant_bit32(uint, rspirv::spirv::Scope::Workgroup as u32);
+        builder
+            .load(
+                uint,
+                None,
+                var,
+                Some(rspirv::spirv::MemoryAccess::MAKE_POINTER_AVAILABLE),
+                [rspirv::dr::Operand::IdScope(scope)],
+            )
+            .unwrap();
+        builder.ret().unwrap();
+        builder.end_function().unwrap();
+
+        let words = builder.module().assemble();
+        let error = words
+            .as_slice()
+            .validate(TargetEnv::Universal1_4)
+            .expect_err("MakePointerAvailable (load) memory access requires SPIR-V 1.5");
+        assert_eq!(
+            error,
+            ValidationError::OperandRequiresSpirvVersion {
+                opcode: rspirv::spirv::Op::Load,
+                operand_index: 1,
+                required_version: SpirvVersion::new(1, 5),
+                target_version: SpirvVersion::new(1, 4),
+            }
+        );
+    }
+
+    #[test]
     fn memory_access_make_pointer_available_requires_vulkan_memory_model_capability() {
         use rspirv::{binary::Assemble, dr::Builder};
 
@@ -21058,6 +21114,62 @@ mod tests {
                 opcode: rspirv::spirv::Op::Store,
                 operand_index: 2,
                 required_capability: rspirv::spirv::Capability::VulkanMemoryModel
+            }
+        );
+    }
+
+    #[test]
+    fn memory_access_make_pointer_visible_load_requires_spirv_1_5() {
+        use rspirv::{binary::Assemble, dr::Builder};
+
+        let mut builder = Builder::new();
+        builder.set_version(1, 4);
+        builder.capability(rspirv::spirv::Capability::Shader);
+        builder.memory_model(
+            rspirv::spirv::AddressingModel::Logical,
+            rspirv::spirv::MemoryModel::GLSL450,
+        );
+
+        let void = builder.type_void();
+        let uint = builder.type_int(32, 0);
+        let ptr = builder.type_pointer(None, rspirv::spirv::StorageClass::Workgroup, uint);
+        let function_type = builder.type_function(void, std::iter::empty::<u32>());
+        let var = builder.variable(ptr, None, rspirv::spirv::StorageClass::Workgroup, None);
+
+        builder
+            .begin_function(
+                void,
+                None,
+                rspirv::spirv::FunctionControl::NONE,
+                function_type,
+            )
+            .unwrap();
+        builder.begin_block(None).unwrap();
+        let scope = builder.constant_bit32(uint, rspirv::spirv::Scope::Workgroup as u32);
+        builder
+            .load(
+                uint,
+                None,
+                var,
+                Some(rspirv::spirv::MemoryAccess::MAKE_POINTER_VISIBLE),
+                [rspirv::dr::Operand::IdScope(scope)],
+            )
+            .unwrap();
+        builder.ret().unwrap();
+        builder.end_function().unwrap();
+
+        let words = builder.module().assemble();
+        let error = words
+            .as_slice()
+            .validate(TargetEnv::Universal1_4)
+            .expect_err("MakePointerVisible (load) memory access requires SPIR-V 1.5");
+        assert_eq!(
+            error,
+            ValidationError::OperandRequiresSpirvVersion {
+                opcode: rspirv::spirv::Op::Load,
+                operand_index: 1,
+                required_version: SpirvVersion::new(1, 5),
+                target_version: SpirvVersion::new(1, 4),
             }
         );
     }
