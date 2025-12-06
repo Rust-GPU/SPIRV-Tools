9a40e43b7447d113e36bde4b4d376c00e04b7fd6
Christian Legnitto <christian@legnitto.com>
2025-12-06 12:09:26 -0800

    Suppress unused parameters in fuzz bridge stub

--- rust/spirv-tools-ffi/src/context_bridge.cc
+++ rust/spirv-tools-ffi/src/context_bridge.cc
@@ -159,6 +159,8 @@ ReduceResult reduce_with_cpp(std::uint32_t env,
 FuzzResult fuzz_with_cpp(std::uint32_t env,
                          rust::Slice<const std::uint32_t> words,
                          const FuzzOptions&) {
+  (void)env;
+  (void)words;
   FuzzResult result{/*success=*/false,
                     ToolError::Disabled,
                     ::rust::String("fuzzer bridge not yet wired to C++"),
