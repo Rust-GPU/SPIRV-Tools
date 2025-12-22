b3cccc367c10b4b3987bcec259a38123552f157e
Christian Legnitto <christian@legnitto.com>
2025-12-21 23:24:00 -0800

    Simplify uge with zero lhs

--- rust/spirv-tools-opt/src/lib.rs
+++ rust/spirv-tools-opt/src/lib.rs
@@ -1433,6 +1433,7 @@ pub fn rewrites() -> Vec<Rewrite<SpirvLang, ()>> {
         rewrite!("ugt-max-right"; "(ugt ?x ?c)" => { BoolConst { value: false } } if is_const_all_ones(var("?c"))),
         rewrite!("ugt-max-left"; "(ugt ?c ?x)" => "(ne ?x ?c)" if is_const_all_ones(var("?c"))),
         rewrite!("uge-zero-right"; "(uge ?x ?c)" => { BoolConst { value: true } } if is_const_zero(var("?c"))),
+        rewrite!("uge-zero-left"; "(uge ?c ?x)" => "(eq ?x ?c)" if is_const_zero(var("?c"))),
         rewrite!("logeq-self"; "(leq ?a ?a)" => { BoolConst { value: true } }),
         rewrite!("logne-self"; "(lne ?a ?a)" => { BoolConst { value: false } }),
         rewrite!("logand-neg"; "(land ?a (lnot ?a))" => { BoolConst { value: false } }),
@@ -13750,5 +13751,6 @@ mod tests {
         assert_simplifies("(ugt x 4294967295)", "false");
         assert_simplifies("(ugt 4294967295 x)", "(ne x 4294967295)");
         assert_simplifies("(uge x 0)", "true");
+        assert_simplifies("(uge 0 x)", "(eq x 0)");
     }
 }
