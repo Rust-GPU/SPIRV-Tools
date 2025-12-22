cc07dbe01bf32723fb281c68b2f286727d0101e6
Christian Legnitto <christian@legnitto.com>
2025-12-21 22:38:37 -0800

    Add logne between dual maxterms

--- rust/spirv-tools-opt/src/lib.rs
+++ rust/spirv-tools-opt/src/lib.rs
@@ -1481,6 +1481,7 @@ pub fn rewrites() -> Vec<Rewrite<SpirvLang, ()>> {
         rewrite!("logeq-minterm-dual"; "(leq (land ?a (lnot ?b)) (land (lnot ?a) ?b))" => "(leq ?a ?b)"),
         rewrite!("logne-minterm-dual"; "(lne (land ?a (lnot ?b)) (land (lnot ?a) ?b))" => "(lne ?a ?b)"),
         rewrite!("logeq-maxterm-dual"; "(leq (lor ?a (lnot ?b)) (lor (lnot ?a) ?b))" => "(leq ?a ?b)"),
+        rewrite!("logne-maxterm-dual"; "(lne (lor ?a (lnot ?b)) (lor (lnot ?a) ?b))" => "(lne ?a ?b)"),
         rewrite!("logeq-or-split-a-right"; "(leq (lor ?a ?b) (lor ?a (lnot ?b)))" => "?a"),
         rewrite!("logeq-or-split-a-left"; "(leq (lor ?a (lnot ?b)) (lor ?a ?b))" => "?a"),
         rewrite!("logeq-or-split-b-right"; "(leq (lor ?a ?b) (lor (lnot ?a) ?b))" => "?b"),
@@ -11199,6 +11200,7 @@ mod tests {
         assert_simplifies("(leq (land a (lnot b)) (land (lnot a) b))", "(leq a b)");
         assert_simplifies("(lne (land a (lnot b)) (land (lnot a) b))", "(lne a b)");
         assert_simplifies("(leq (lor a (lnot b)) (lor (lnot a) b))", "(leq a b)");
+        assert_simplifies("(lne (lor a (lnot b)) (lor (lnot a) b))", "(lne a b)");
     }
 
     #[test]
