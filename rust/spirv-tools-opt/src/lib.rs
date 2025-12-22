0f8e708e75c93c2b03979f3e534e3525d2886945
Christian Legnitto <christian@legnitto.com>
2025-12-21 23:21:19 -0800

    Simplify ule with max lhs

--- rust/spirv-tools-opt/src/lib.rs
+++ rust/spirv-tools-opt/src/lib.rs
@@ -1427,6 +1427,7 @@ pub fn rewrites() -> Vec<Rewrite<SpirvLang, ()>> {
         rewrite!("ule-zero-right"; "(ule ?x ?c)" => "(eq ?x ?c)" if is_const_zero(var("?c"))),
         rewrite!("ule-zero-left"; "(ule ?c ?x)" => { BoolConst { value: true } } if is_const_zero(var("?c"))),
         rewrite!("ule-max-right"; "(ule ?x ?c)" => { BoolConst { value: true } } if is_const_all_ones(var("?c"))),
+        rewrite!("ule-max-left"; "(ule ?c ?x)" => "(eq ?x ?c)" if is_const_all_ones(var("?c"))),
         rewrite!("logeq-self"; "(leq ?a ?a)" => { BoolConst { value: true } }),
         rewrite!("logne-self"; "(lne ?a ?a)" => { BoolConst { value: false } }),
         rewrite!("logand-neg"; "(land ?a (lnot ?a))" => { BoolConst { value: false } }),
@@ -13738,5 +13739,6 @@ mod tests {
         assert_simplifies("(ule x 0)", "(eq x 0)");
         assert_simplifies("(ule 0 x)", "true");
         assert_simplifies("(ule x 4294967295)", "true");
+        assert_simplifies("(ule 4294967295 x)", "(eq x 4294967295)");
     }
 }
