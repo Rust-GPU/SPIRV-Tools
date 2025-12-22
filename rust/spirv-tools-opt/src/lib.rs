335311322763a3a047e200a4522afbe8df69fd8e
Christian Legnitto <christian@legnitto.com>
2025-12-21 23:30:54 -0800

    Simplify sgt with max rhs

--- rust/spirv-tools-opt/src/lib.rs
+++ rust/spirv-tools-opt/src/lib.rs
@@ -1446,6 +1446,7 @@ pub fn rewrites() -> Vec<Rewrite<SpirvLang, ()>> {
         rewrite!("sle-max-left"; "(sle ?c ?x)" => "(eq ?x ?c)" if is_const_signed_max(var("?c"))),
         rewrite!("sgt-min-right"; "(sgt ?x ?c)" => "(ne ?x ?c)" if is_const_signed_min(var("?c"))),
         rewrite!("sgt-min-left"; "(sgt ?c ?x)" => { BoolConst { value: false } } if is_const_signed_min(var("?c"))),
+        rewrite!("sgt-max-right"; "(sgt ?x ?c)" => { BoolConst { value: false } } if is_const_signed_max(var("?c"))),
         rewrite!("logeq-self"; "(leq ?a ?a)" => { BoolConst { value: true } }),
         rewrite!("logne-self"; "(lne ?a ?a)" => { BoolConst { value: false } }),
         rewrite!("logand-neg"; "(land ?a (lnot ?a))" => { BoolConst { value: false } }),
@@ -13810,5 +13811,6 @@ mod tests {
         assert_simplifies("(sle 2147483647 x)", "(eq x 2147483647)");
         assert_simplifies("(sgt x 2147483648)", "(ne x 2147483648)");
         assert_simplifies("(sgt 2147483648 x)", "false");
+        assert_simplifies("(sgt x 2147483647)", "false");
     }
 }
