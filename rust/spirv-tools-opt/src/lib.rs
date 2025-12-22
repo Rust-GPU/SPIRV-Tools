9f13408730bfe3865fd4c44d429bacdc645cbb31
Christian Legnitto <christian@legnitto.com>
2025-12-21 21:33:05 -0800

    Add xor and-or noty fold

--- rust/spirv-tools-opt/src/lib.rs
+++ rust/spirv-tools-opt/src/lib.rs
@@ -929,6 +929,8 @@ pub fn rewrites() -> Vec<Rewrite<SpirvLang, ()>> {
         rewrite!("bxor-or-band-notx-left"; "(bxor (bor ?x ?y) (band ?y (bnot ?x)))" => "?x"),
         rewrite!("bxor-band-or-notx-right"; "(bxor (band ?x ?y) (bor (bnot ?x) ?y))" => "(bnot ?x)"),
         rewrite!("bxor-band-or-notx-left"; "(bxor (band ?x ?y) (bor ?y (bnot ?x)))" => "(bnot ?x)"),
+        rewrite!("bxor-band-or-noty-right"; "(bxor (band ?x ?y) (bor ?x (bnot ?y)))" => "(bnot ?y)"),
+        rewrite!("bxor-band-or-noty-left"; "(bxor (band ?x ?y) (bor (bnot ?y) ?x))" => "(bnot ?y)"),
         rewrite!("bxor-band-bnot-bnot-right"; "(bxor (band ?x ?y) (band (bnot ?x) (bnot ?y)))" => "(bnot (bxor ?x ?y))"),
         rewrite!("bxor-band-bnot-bnot-left"; "(bxor (band ?x ?y) (band (bnot ?y) (bnot ?x)))" => "(bnot (bxor ?x ?y))"),
         rewrite!("bxor-bnot-bnot"; "(bxor (bnot ?x) (bnot ?y))" => "(bxor ?x ?y)"),
@@ -10137,6 +10139,24 @@ mod tests {
         assert!(
             is_bnot_named_symbol(&runner.egraph, root, "x"),
             "expected (x & y) ^ (~x | y) to rewrite to ~x"
+        );
+    }
+
+    #[test]
+    fn rewrites_bxor_band_or_noty_to_not_y() {
+        let expr = RecExpr::from(vec![
+            SpirvLang::Symbol(Symbol::from("x")), // 0
+            SpirvLang::Symbol(Symbol::from("y")), // 1
+            SpirvLang::BitAnd([Id::from(0), Id::from(1)]),
+            SpirvLang::BitNot(Id::from(1)),
+            SpirvLang::BitOr([Id::from(0), Id::from(3)]),
+            SpirvLang::BitXor([Id::from(2), Id::from(4)]),
+        ]);
+        let runner = Runner::default().with_expr(&expr).run(&rewrites());
+        let root = runner.roots[0];
+        assert!(
+            is_bnot_named_symbol(&runner.egraph, root, "y"),
+            "expected (x & y) ^ (x | ~y) to rewrite to ~y"
         );
     }
 
