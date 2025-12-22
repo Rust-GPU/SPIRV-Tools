0d916d0ad66b564e6aba1595c4561a5ad7f00991
Christian Legnitto <christian@legnitto.com>
2025-12-21 22:31:27 -0800

    Add logne against negated or

--- rust/spirv-tools-opt/src/lib.rs
+++ rust/spirv-tools-opt/src/lib.rs
@@ -1467,6 +1467,7 @@ pub fn rewrites() -> Vec<Rewrite<SpirvLang, ()>> {
         rewrite!("logeq-land-negself"; "(leq ?a (land (lnot ?a) ?b))" => "(land (lnot ?a) ?b)"),
         rewrite!("logne-land-negself"; "(lne ?a (land (lnot ?a) ?b))" => "(lor ?a (lnot ?b))"),
         rewrite!("logeq-lor-negself"; "(leq ?a (lor (lnot ?a) ?b))" => "(land ?a ?b)"),
+        rewrite!("logne-lor-negself"; "(lne ?a (lor (lnot ?a) ?b))" => "(lor (lnot ?a) (lnot ?b))"),
         rewrite!("logeq-or-split-a-right"; "(leq (lor ?a ?b) (lor ?a (lnot ?b)))" => "?a"),
         rewrite!("logeq-or-split-a-left"; "(leq (lor ?a (lnot ?b)) (lor ?a ?b))" => "?a"),
         rewrite!("logeq-or-split-b-right"; "(leq (lor ?a ?b) (lor (lnot ?a) ?b))" => "?b"),
@@ -11171,6 +11172,7 @@ mod tests {
         assert_simplifies("(leq a (land (lnot a) b))", "(land (lnot a) b)");
         assert_simplifies("(lne a (land (lnot a) b))", "(lor a (lnot b))");
         assert_simplifies("(leq a (lor (lnot a) b))", "(land a b)");
+        assert_simplifies("(lne a (lor (lnot a) b))", "(lor (lnot a) (lnot b))");
     }
 
     #[test]
