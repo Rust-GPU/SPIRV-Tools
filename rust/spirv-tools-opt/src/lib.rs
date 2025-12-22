3924a8c8c7b060a62d9917516a6b4eb653e0d6bf
Christian Legnitto <christian@legnitto.com>
2025-12-21 22:27:17 -0800

    Add logor eq absorption for negated lhs comm

--- rust/spirv-tools-opt/src/lib.rs
+++ rust/spirv-tools-opt/src/lib.rs
@@ -1458,6 +1458,7 @@ pub fn rewrites() -> Vec<Rewrite<SpirvLang, ()>> {
         rewrite!("logand-lne-absorb-not-comm"; "(land (lnot ?a) (lne ?b ?a))" => "(land (lnot ?a) ?b)"),
         rewrite!("logor-leq-absorb-comm"; "(lor ?a (leq ?b ?a))" => "(lor ?a (lnot ?b))"),
         rewrite!("logor-lne-absorb-comm"; "(lor ?a (lne ?b ?a))" => "(lor ?a ?b)"),
+        rewrite!("logor-leq-absorb-not-comm"; "(lor (lnot ?a) (leq ?b ?a))" => "(lor (lnot ?a) ?b)"),
         rewrite!("logeq-or-split-a-right"; "(leq (lor ?a ?b) (lor ?a (lnot ?b)))" => "?a"),
         rewrite!("logeq-or-split-a-left"; "(leq (lor ?a (lnot ?b)) (lor ?a ?b))" => "?a"),
         rewrite!("logeq-or-split-b-right"; "(leq (lor ?a ?b) (lor (lnot ?a) ?b))" => "?b"),
@@ -11153,6 +11154,7 @@ mod tests {
         assert_simplifies("(land (lnot a) (lne b a))", "(land (lnot a) b)");
         assert_simplifies("(lor a (leq b a))", "(lor a (lnot b))");
         assert_simplifies("(lor a (lne b a))", "(lor a b)");
+        assert_simplifies("(lor (lnot a) (leq b a))", "(lor (lnot a) b)");
     }
 
     #[test]
